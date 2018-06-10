78230203a1b641e77c04aa837bd50b4b4a2dadba
蒋亚楠 <jiangyanan@jiangyanandeMacBook-Air.local>
2018-06-10 16:19:31 +0800

    cache-control demo

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const config = require('./config');
 
 var server = http.createServer(function(request,response){
     var pathName = url.parse(request.url).pathname;
-    var realPath = 'assets' +pathName;
+    var realPath = 'assets' + pathName;
     var ext = path.extname(realPath);
     ext = ext ? ext.slice(1) : 'unknown';
     var contentType = mime[ext] || 'text/plain';
-
+    
     if(ext.match(config.Expires.fileMatch)){
         var expires = new Date();
         expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
@@ -22,8 +22,6 @@ var server = http.createServer(function(request,response){
     fs.stat(realPath,function(err,stat){
         var lastModified = stat.mtime.toUTCString();
         response.setHeader('Last-Modified',lastModified);
-        console.log(lastModified);
-        console.log(request.headers['if-modified-since'])
         if(request.headers['if-modified-since']&&lastModified ==request.headers['if-modified-since']){
             response.writeHead(304,'Not Modified');
             response.end();
@@ -37,7 +35,9 @@ var server = http.createServer(function(request,response){
                     response.end()
                 }else{
                     fs.readFile(realPath,function(err,file){
+                        console.log(err)
                         if(err){
+                            console.log(11111)
                             response.writeHead(500,{
                                 'Content-Type' : 'text/plain'
                             })
